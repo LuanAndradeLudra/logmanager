bf116abf8117b7f4bcf8275d7a285a019f9185e5
Luan Andrade <luanandradeti10@gmail.com>
2026-06-12 00:19:01 -0300

    feat: add indexes to orders table

---
+++ logmanager-test/database/migrations/2026_06_12_031307_add_indexes_to_orders_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddIndexesToOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->index(['driver_id', 'created_at']);
+            $table->index('status');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->dropIndex(['driver_id', 'created_at']);
+            $table->dropIndex('status');
+        });
+    }
+}
