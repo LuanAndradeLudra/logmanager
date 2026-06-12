bced9286b996b1abe31f0cda12cd2b92b98a5485
Luan Andrade <luanandradeti10@gmail.com>
2026-06-12 00:06:13 -0300

    feat: add Driver and Order

---
+++ logmanager-test/database/factories/Infrastructure/Persistence/Eloquent/Models/OrderFactory.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Factories\Infrastructure\Persistence\Eloquent\Models;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class OrderFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
